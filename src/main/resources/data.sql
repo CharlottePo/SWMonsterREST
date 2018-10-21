0809e30782481442cf4acf6b946c36bd57c1ff3b
guinardpaul <guinardpaul@gmail.com>
2018-10-21 21:00:45 +0200

    [ENHANCEMENT][DATA, SCHEMA] add more dummy data for front end test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,13 @@
 -- INIT monsters
 INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Veromos', 5, 5, 35, 6780, 565, 557, 100, 0.15, 0.15, 0.25, 0);
 INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Belladeon', 3, 5, 35, 7260, 347, 500, 108, 0.15, 0.15, 0.25, 0);
-INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Chasun', 4, 5, 16, 6030, 360, 318, 101, 0.35, 0.15, 0.15, 0);
+INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Chasun', 4, 5, 16, 6030, 360, 318, 101, 0.15, 0.15, 0.15, 0);
+INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Shannon', 3, 5, 35, 6420, 371, 412, 111, 0.15, 0.15, 0.15, 0);
+INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Bernard', 3, 5, 35, 7635, 307, 517, 111, 0.15, 0.15, 0.15, 0);
+INSERT INTO monsters (name, nat_level, nb_stars, level, base_pv, base_atq, base_def, base_vit, base_tx_crit, base_dgt_crit, base_res, base_prec) VALUES ('Colleen', 3, 5, 35, 7620, 324, 324, 106, 0.15, 0.15, 0.15, 0);
 
 
 -- INIT runes
+INSERT INTO runes (numRune, bonus, nb_stars, level, type_rune) VALUES (1, 255, 6, 15, 'Energy');
+INSERT INTO runes (numRune, bonus, nb_stars, level, type_rune) VALUES (2,12, 6, 15, 'Despair');
+INSERT INTO runes (numRune, bonus, nb_stars, level, type_rune) VALUES (6, 145, 6, 15, 'Will');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 -- DROP tables
 DROP TABLE IF EXISTS monsters;
-
+DROP TABLE IF EXISTS runes;
 
 -- CREATE tables
 CREATE TABLE monsters (
@@ -17,4 +17,13 @@ base_tx_crit FLOAT,
 base_dgt_crit FLOAT, 
 base_res FLOAT, 
 base_prec FLOAT
+);
+
+CREATE TABLE runes (
+id SERIAL,
+numRune INTEGER,
+bonus VARCHAR(255),
+nb_stars INTEGER,
+level INTEGER,
+type_rune VARCHAR(255)
 );
